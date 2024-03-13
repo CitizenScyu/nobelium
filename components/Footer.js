d431426944ac1060f88dcf794c8722fbc3e07ee6
CitizenScyu <58416773+CitizenScyu@users.noreply.github.com>
2024-03-13 12:47:10 +0800

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -18,6 +18,23 @@ const Footer = ({ fullWidth }) => {
           <p>
             © {BLOG.author} {from === y || !from ? y : `${from} - ${y}`}
           </p>
+        <a href="https://t.me/CitizenScyu" className="flex items-center">
+        <span className="mr-1">五星上将麦克阿象</span>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="w-4 h-4"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M9 5l7 7-7 7"
+          />
+        </svg>
+      </a> 
         </div>
       </div>
     </div>
