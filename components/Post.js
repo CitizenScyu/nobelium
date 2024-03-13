1ea17993befed7cdc3626759e8dbaf852bee04a0
CitizenScyu <58416773+CitizenScyu@users.noreply.github.com>
2024-03-13 12:29:54 +0800

    Update Post.js

--- components/Post.js
+++ components/Post.js
@@ -43,7 +43,7 @@ export default function Post (props) {
                 alt={BLOG.author}
                 width={24}
                 height={24}
-                src={`https://raw.githubusercontent.com/CitizenScyu/nobelium/main/public/favicon_.png`}
+                src="/favicon.ico"
                 className="rounded-full"
               />
               <p className="ml-2 md:block">{BLOG.author}</p>
