2db3801f8a86b9a2135e7d32267b1064058ac3cf
CitizenScyu <58416773+CitizenScyu@users.noreply.github.com>
2024-03-13 12:57:05 +0800

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -22,30 +22,48 @@ const Footer = ({ fullWidth }) => {
         <span className="mr-1">五星上将麦克阿象</span>
         <svg
   xmlns="http://www.w3.org/2000/svg"
-  className="w-4 h-4"
   viewBox="0 0 24 24"
   fill="none"
   stroke="currentColor"
 >
   <path
-    strokeLinecap="round"
-    strokeLinejoin="round"
-    strokeWidth={2}
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    stroke-width="2"
     d="M12 19c-4.4183 0-8-3.5817-8-8s3.5817-8 8-8 8 3.5817 8 8-3.5817 8-8 8z"
   />
   <path
-    strokeLinecap="round"
-    strokeLinejoin="round"
-    strokeWidth={2}
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    stroke-width="2"
     d="M8 12h8"
   />
   <path
-    strokeLinecap="round"
-    strokeLinejoin="round"
-    strokeWidth={2}
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    stroke-width="2"
     d="M12 8v8"
   />
+  <path
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    stroke-width="2"
+    d="M12 4v4"
+  />
+  <path
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    stroke-width="2"
+    d="M4 12h4"
+  />
+  <path
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    stroke-width="2"
+    d="M20 12h4"
+  />
 </svg>
+
       </a> 
         </div>
       </div>
